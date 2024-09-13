a0bc9f9b516e76bae2f171b6d785c6b827bb3797
moses onorefe <onorefemoses@gmail.com>
2024-09-14 00:09:43 +0100

    commit

---
+++ switch.js
@@ -0,0 +1,24 @@
+let Days = "Thursday"
+
+switch (Days) {
+    case  "Monday":
+        document.write("Monday is the second day of the week")
+        break;
+    case "Tuesday":
+    document.write("Tuesday is the third day of the week")
+    break;
+    case "Wednessday" :
+        document.write("Wednessday is the fourth day of the week")
+     break;
+     case "Thursday" :
+        document.write("Thursday is the fifth day of the week")
+        break;
+     case "Friday" :
+            document.write("Friday is the sixth day of the week")
+         break;
+     case "Saturday" :
+            document.write("Saturday is the seventh day of the week")
+         break;    
+    default:
+        document.write("Sorry days of the week is not found")
+}
